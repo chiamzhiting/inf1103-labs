c5277b7792de136dbeff7ba43a566b25b498389e
Chiam Zhi Ting <2605365@sit.singaporetech.edu.sg>
2026-09-08 15:38:15 +0800

    First Post

---
+++ hello.py
@@ -0,0 +1,48 @@
+#Activity 1
+#Q1 #done
+#Q2
+print("===========================")
+print("Welcome here")
+print("My first post!")
+print("===========================")
+
+#Q2
+##a. yes 
+##b. top to bottom
+##c. output appear in terminal 
+
+#Q4 #git add
+
+#
+#
+#
+
+#Activity 2
+#Q1
+# print("===========================")
+# print("Welcome here")
+# print("My first post!")
+# print("===========================")
+
+# username = "cool_creator"
+# bio = "Fun Blogger"
+# followers = 100
+
+# print("Username:", username)
+# print("Bio:", bio)
+# print("Followers:", followers)
+
+# #Q2
+# ##a. variables are used to store profile information about the profile
+# ##b. if the values changes, the printed output will also change to show the new information.
+# ##c. output appear in the terminal 
+
+# #Q3 #done
+# #Q4 #done
+
+# #Activity 3
+# followers = 100
+
+# followers += 50
+# print("Day1:", followers)
+
