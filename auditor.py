760b90efdf2fbf032cc63812406644f0c9cfc6e4
Chiam Zhi Ting <2605365@sit.singaporetech.edu.sg>
2026-09-17 14:09:30 +0800

    Week 2 Lab

---
+++ auditor.py
@@ -0,0 +1,24 @@
+inventory = 0
+while True:
+    entry = input("Enter a stock quantity (or type 'quit' to exit): ")
+
+    if entry.lower() == "quit":
+        print(f"Total Units Processed: {inventory}")
+        break
+
+    if not entry.isdigit():
+        print("Error: Please enter a valid integer.")
+        continue
+
+    quantity = int(entry)
+
+    if quantity < 0:
+        print("Error: Negative values are not allowed.")
+        continue
+
+    inventory += quantity
+    if inventory > 500:
+        print("Alert: Inventory exceeds 500 units! Stopping audit")
+        print(f"Final Total Units Processed: {inventory}")
+        break
+    
